90bf1a2a70c1354224a24d40f9bdb009f35c3a5b
KernelMaker <songzhao.asm@icloud.com>
2015-07-16 14:51:40 +0800

    remove interfaces to one ccfile

--- src/pika_kv.cc
+++ src/pika_kv.cc
@@ -53,3 +53,30 @@ void SetCmd::Do(std::list<std::string> &argv, std::string &ret) {
         ret.append("\r\n");
     }
 }
+
+void GetCmd::Do(std::list<std::string> &argv, std::string &ret) {
+    if ((arity > 0 && argv.size() != arity) || (arity < 0 && argv.size() < -arity)) {
+        ret = "-ERR wrong number of arguments for ";
+        ret.append(argv.front());
+        ret.append(" command\r\n");
+        return;
+    }
+    argv.pop_front();
+    std::string key = argv.front();
+    argv.pop_front();
+    std::string value;
+    nemo::Status s = g_pikaServer->GetHandle()->Get(key, &value);
+    if (s.ok()) {
+        char buf[32];
+        snprintf(buf, sizeof(buf), "$%d\r\n", (int)value.size());
+        ret.append(buf);
+        ret.append(value.data(), value.size());
+        ret.append("\r\n");
+    } else if (s.IsNotFound()) {
+        ret.append("$-1\r\n");
+    } else {
+        ret.append("-ERR ");
+        ret.append(s.ToString().c_str());
+        ret.append("\r\n");
+    }
+}
