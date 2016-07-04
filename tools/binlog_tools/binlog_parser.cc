df91aa1c459493efc3f39257be9533edafd4b2b9
wenduo <hrxwwd@163.com>
2016-07-04 17:02:55 +0800

    binlog_parser delete useless pointer

--- tools/binlog_tools/binlog_parser.cc
+++ tools/binlog_tools/binlog_parser.cc
@@ -191,6 +191,7 @@ int main(int argc, char *argv[]) {
   Binlog* new_logger = new Binlog(output_path, INPUT_FILESIZE);
   std::cout << "test result:new binlog can be loaded " << std::endl;
   delete binlog_consumer;
+  delete binlog_producer;
   delete old_logger;
   delete new_logger;
   return 0;
