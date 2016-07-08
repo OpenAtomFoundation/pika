d6aa61d680b14a8f98d0500205662d2126b9421f
zhaoanan <zhaoanan@360.cn>
2016-07-08 15:55:51 +0800

    update nemo

--- release.sh
+++ release.sh
@@ -2,9 +2,11 @@
 
 # Used for Release only.
 
-TAG="2.0.4"
+TAG="2.1.0"
+make clean && make RPATH=./lib
 
-make clean && make RPATH=./lib __REL=1
+#TAG="2.0.4"
+#make clean && make RPATH=./lib __REL=1
 
 #cp LOOKME output/
 #tar -czf $RELEASE_FILE output
