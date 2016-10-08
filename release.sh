558c8cac406a565edb31d8e7f41abde196a7fc57
KernelMaker <songzhao.asm@icloud.com>
2016-10-08 10:49:21 +0800

    change release.sh

--- release.sh
+++ release.sh
@@ -2,12 +2,7 @@
 
 # Used for Release only.
 
-TAG="2.1.0"
+TAG="2.1.2"
 make clean && make RPATH=./lib __REL=1
 
-#TAG="2.0.4"
-#make clean && make RPATH=./lib __REL=1
-
-#cp LOOKME output/
-#tar -czf $RELEASE_FILE output
 tar -czf pika${TAG}_bin.tar.gz output
