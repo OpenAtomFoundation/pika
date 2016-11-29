d48918316addf39d0ea4450c30032682a170acda
baotiao <baotiao@gmail.com>
2016-11-30 02:10:57 +0800

    add copyright

--- include/pika_admin.h
+++ include/pika_admin.h
@@ -1,3 +1,8 @@
+// Copyright (c) 2015-present, Qihoo, Inc.  All rights reserved.
+// This source code is licensed under the BSD-style license found in the
+// LICENSE file in the root directory of this source tree. An additional grant
+// of patent rights can be found in the PATENTS file in the same directory.
+
 #ifndef PIKA_ADMIN_H_
 #define PIKA_ADMIN_H_
 #include "pika_command.h"
