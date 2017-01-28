aced840eee6be9a94ca881f7831febfdc9aa6a16
leviathan1995 <leviathan1995@outlook.com>
2017-01-28 16:54:25 +0800

    add header file

--- src/pika_command.cc
+++ src/pika_command.cc
@@ -4,6 +4,7 @@
 // of patent rights can be found in the PATENTS file in the same directory.
 
 #include "pika_admin.h"
+#include "pika_slot.h"
 #include "pika_kv.h"
 #include "pika_hash.h"
 #include "pika_list.h"
@@ -54,7 +55,7 @@ void InitCmdInfoTable() {
   CmdInfo* dbsizeptr = new CmdInfo(kCmdNameDbsize, 1, kCmdFlagsRead | kCmdFlagsAdmin);
   cmd_infos.insert(std::pair<std::string, CmdInfo*>(kCmdNameDbsize, dbsizeptr));
 
-    
+
   //migrate slot
   CmdInfo* slotmgrtslotptr = new CmdInfo(kCmdNameSlotsMgrtSlot, 5, kCmdFlagsRead | kCmdFlagsAdmin);
   cmd_infos.insert(std::pair<std::string, CmdInfo*>(kCmdNameSlotsMgrtSlot, slotmgrtslotptr));
@@ -76,8 +77,8 @@ void InitCmdInfoTable() {
   cmd_infos.insert(std::pair<std::string, CmdInfo*>(kCmdNameSlotsDel, slotsdelptr));
   CmdInfo* slotsscanptr = new CmdInfo(kCmdNameSlotsScan, -3, kCmdFlagsRead | kCmdFlagsAdmin);
   cmd_infos.insert(std::pair<std::string, CmdInfo*>(kCmdNameSlotsScan, slotsscanptr));
-    
-    
+
+
   //Kv
   ////SetCmd
   CmdInfo* setptr = new CmdInfo(kCmdNameSet, -3, kCmdFlagsWrite | kCmdFlagsKv);
@@ -461,7 +462,7 @@ void InitCmdTable(std::unordered_map<std::string, Cmd*> *cmd_table) {
   cmd_table->insert(std::pair<std::string, Cmd*>(kCmdNameSlotsDel, slotsdelptr));
   Cmd* slotsscanptr = new SlotsScanCmd();
   cmd_table->insert(std::pair<std::string, Cmd*>(kCmdNameSlotsScan, slotsscanptr));
-    
+
   //Kv
   ////SetCmd
   Cmd* setptr = new SetCmd();
@@ -553,49 +554,49 @@ void InitCmdTable(std::unordered_map<std::string, Cmd*> *cmd_table) {
   //Hash
   ////HDelCmd
   Cmd* hdelptr = new HDelCmd();
-  cmd_table->insert(std::pair<std::string, Cmd*>(kCmdNameHDel, hdelptr));  
+  cmd_table->insert(std::pair<std::string, Cmd*>(kCmdNameHDel, hdelptr));
   ////HSetCmd
   Cmd* hsetptr = new HSetCmd();
-  cmd_table->insert(std::pair<std::string, Cmd*>(kCmdNameHSet, hsetptr));  
+  cmd_table->insert(std::pair<std::string, Cmd*>(kCmdNameHSet, hsetptr));
   ////HGetCmd
   Cmd* hgetptr = new HGetCmd();
-  cmd_table->insert(std::pair<std::string, Cmd*>(kCmdNameHGet, hgetptr));  
+  cmd_table->insert(std::pair<std::string, Cmd*>(kCmdNameHGet, hgetptr));
   ////HGetallCmd
   Cmd* hgetallptr = new HGetallCmd();
-  cmd_table->insert(std::pair<std::string, Cmd*>(kCmdNameHGetall, hgetallptr));  
+  cmd_table->insert(std::pair<std::string, Cmd*>(kCmdNameHGetall, hgetallptr));
   ////HExistsCmd
   Cmd* hexistsptr = new HExistsCmd();
-  cmd_table->insert(std::pair<std::string, Cmd*>(kCmdNameHExists, hexistsptr));  
+  cmd_table->insert(std::pair<std::string, Cmd*>(kCmdNameHExists, hexistsptr));
   ////HIncrbyCmd
   Cmd* hincrbyptr = new HIncrbyCmd();
-  cmd_table->insert(std::pair<std::string, Cmd*>(kCmdNameHIncrby, hincrbyptr));  
+  cmd_table->insert(std::pair<std::string, Cmd*>(kCmdNameHIncrby, hincrbyptr));
   ////HIncrbyfloatCmd
   Cmd* hincrbyfloatptr = new HIncrbyfloatCmd();
-  cmd_table->insert(std::pair<std::string, Cmd*>(kCmdNameHIncrbyfloat, hincrbyfloatptr));  
+  cmd_table->insert(std::pair<std::string, Cmd*>(kCmdNameHIncrbyfloat, hincrbyfloatptr));
   ////HKeysCmd
   Cmd* hkeysptr = new HKeysCmd();
-  cmd_table->insert(std::pair<std::string, Cmd*>(kCmdNameHKeys, hkeysptr));  
+  cmd_table->insert(std::pair<std::string, Cmd*>(kCmdNameHKeys, hkeysptr));
   ////HLenCmd
   Cmd* hlenptr = new HLenCmd();
-  cmd_table->insert(std::pair<std::string, Cmd*>(kCmdNameHLen, hlenptr));  
+  cmd_table->insert(std::pair<std::string, Cmd*>(kCmdNameHLen, hlenptr));
   ////HMgetCmd
   Cmd* hmgetptr = new HMgetCmd();
-  cmd_table->insert(std::pair<std::string, Cmd*>(kCmdNameHMget, hmgetptr));  
+  cmd_table->insert(std::pair<std::string, Cmd*>(kCmdNameHMget, hmgetptr));
   ////HMsetCmd
   Cmd* hmsetptr = new HMsetCmd();
-  cmd_table->insert(std::pair<std::string, Cmd*>(kCmdNameHMset, hmsetptr));  
+  cmd_table->insert(std::pair<std::string, Cmd*>(kCmdNameHMset, hmsetptr));
   ////HSetnxCmd
   Cmd* hsetnxptr = new HSetnxCmd();
-  cmd_table->insert(std::pair<std::string, Cmd*>(kCmdNameHSetnx, hsetnxptr));  
+  cmd_table->insert(std::pair<std::string, Cmd*>(kCmdNameHSetnx, hsetnxptr));
   ////HStrlenCmd
   Cmd* hstrlenptr = new HStrlenCmd();
-  cmd_table->insert(std::pair<std::string, Cmd*>(kCmdNameHStrlen, hstrlenptr));  
+  cmd_table->insert(std::pair<std::string, Cmd*>(kCmdNameHStrlen, hstrlenptr));
   ////HValsCmd
   Cmd* hvalsptr = new HValsCmd();
-  cmd_table->insert(std::pair<std::string, Cmd*>(kCmdNameHVals, hvalsptr));  
+  cmd_table->insert(std::pair<std::string, Cmd*>(kCmdNameHVals, hvalsptr));
   ////HScanCmd
   Cmd* hscanptr = new HScanCmd();
-  cmd_table->insert(std::pair<std::string, Cmd*>(kCmdNameHScan, hscanptr));  
+  cmd_table->insert(std::pair<std::string, Cmd*>(kCmdNameHScan, hscanptr));
   //List
   Cmd* lindexptr = new LIndexCmd();
   cmd_table->insert(std::pair<std::string, Cmd*>(kCmdNameLIndex, lindexptr));
@@ -604,7 +605,7 @@ void InitCmdTable(std::unordered_map<std::string, Cmd*> *cmd_table) {
   Cmd* llenptr = new LLenCmd();
   cmd_table->insert(std::pair<std::string, Cmd*>(kCmdNameLLen, llenptr));
   Cmd* lpopptr = new LPopCmd();
-  cmd_table->insert(std::pair<std::string, Cmd*>(kCmdNameLPop, lpopptr));  
+  cmd_table->insert(std::pair<std::string, Cmd*>(kCmdNameLPop, lpopptr));
   Cmd* lpushptr = new LPushCmd();
   cmd_table->insert(std::pair<std::string, Cmd*>(kCmdNameLPush, lpushptr));
   Cmd* lpushxptr = new LPushxCmd();
@@ -768,7 +769,7 @@ void InitCmdTable(std::unordered_map<std::string, Cmd*> *cmd_table) {
 
 }
 
-Cmd* GetCmdFromTable(const std::string& opt, 
+Cmd* GetCmdFromTable(const std::string& opt,
     const std::unordered_map<std::string, Cmd*> &cmd_table) {
   std::unordered_map<std::string, Cmd*>::const_iterator it = cmd_table.find(opt);
   if (it != cmd_table.end()) {
