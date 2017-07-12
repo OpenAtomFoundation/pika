55b9c1be93b984591a7afe838683186b19e06669
Leviathan <leviathan0992@gmail.com>
2017-07-12 09:46:39 +0800

    Create migrator_thread.h

---
+++ tools/pika_to_redis/migrator_thread.h
@@ -0,0 +1,34 @@
+#ifndef MIGRATOR_THREAD_H_
+#define MIGRATOR_THREAD_H_
+
+#include "nemo.h"
+#include "pink/include/redis_cli.h"
+#include "parse_thread.h"
+
+class MigratorThread : public pink::Thread {
+ public:
+  MigratorThread(nemo::Nemo *db, std::vector<ParseThread*> &parsers, char type) :
+      db_(db),
+      parsers_(parsers),
+      type_(type),
+      thread_index_(0),
+      num_thread_(parsers.size()){
+      }
+
+  virtual ~ MigratorThread();
+  bool should_exit_;
+ private:
+  nemo::Nemo *db_;
+  std::vector<ParseThread*> parsers_;
+  char type_;
+  int thread_index_;
+  int num_thread_;
+
+  static std::string GetKey(const rocksdb::Iterator *it);
+  void MigrateDB(char type);
+  void ComposeCmd(nemo::Iterator * iter, char type);
+  void DispatchKey(const std::string &key, char type);
+
+  virtual void *ThreadMain();
+};
+#endif
