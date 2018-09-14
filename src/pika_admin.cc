4ac7e9b15230a9b856e88a9e8cce54bb5f3c2781
fancyrabbit <fancyrabbit@gmail.com>
2018-09-14 23:26:04 +0800

    config get/set * count err fix

--- src/pika_admin.cc
+++ src/pika_admin.cc
@@ -1302,7 +1302,7 @@ void ConfigCmd::ConfigGet(std::string &ret) {
 void ConfigCmd::ConfigSet(std::string& ret) {
   std::string set_item = config_args_v_[1];
   if (set_item == "*") {
-    ret = "*23\r\n";
+    ret = "*24\r\n";
     EncodeString(&ret, "loglevel");
     EncodeString(&ret, "timeout");
     EncodeString(&ret, "requirepass");
