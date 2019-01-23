009fd5ef3d2ab32993c817cac207f6f87372a932
whoiami <andrewmhzhao@gmail.com>
2019-01-23 18:39:37 +0800

    Update Pink (#468)

--- include/pika_dispatch_thread.h
+++ include/pika_dispatch_thread.h
@@ -32,7 +32,7 @@ class PikaDispatchThread {
     virtual std::shared_ptr<pink::PinkConn> NewPinkConn(
         int connfd,
         const std::string &ip_port,
-        pink::ServerThread* server_thread,
+        pink::Thread* server_thread,
         void* worker_specific_data,
         pink::PinkEpoll* pink_epoll) const {
       return std::make_shared<PikaClientConn>(connfd, ip_port, server_thread, pink_epoll, pink::HandleType::kAsynchronous);
