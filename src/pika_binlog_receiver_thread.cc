3c8170a2f8cd2eb32cdd7f6381f20753566bf12d
Zhao Minghuan <andrewmhzhao@gmail.com>
2019-09-12 11:29:36 +0800

    Update Network Layer Pink many bugfix (#749)
    Add TCP_NODELAY to avoid tcp 40ms delay
    Fix coredump
    Fix deadlock
    Fix redis parser parse error

--- src/pika_binlog_receiver_thread.cc
+++ src/pika_binlog_receiver_thread.cc
@@ -18,7 +18,7 @@ PikaBinlogReceiverThread::PikaBinlogReceiverThread(const std::set<std::string> &
         serial_(0) {
   cmds_.reserve(300);
   InitCmdTable(&cmds_);
-  thread_rep_ = pink::NewHolyThread(ips, port, &conn_factory_,
+  thread_rep_ = pink::NewHolyThread(ips, port, &conn_factory_, false,
                                     cron_interval, &handles_);
   thread_rep_->set_thread_name("BinlogReceiver");
 }
