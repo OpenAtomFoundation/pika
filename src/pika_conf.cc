337577ec9ddd0aa054286b87ff92b33a40a9f86d
kernelai <redtomato158@gmail.com>
2019-11-11 11:37:15 +0800

    bugfix limit server-id max to 65535

--- src/pika_conf.cc
+++ src/pika_conf.cc
@@ -114,6 +114,8 @@ int PikaConf::Load()
   GetConfStr("server-id", &server_id_);
   if (server_id_.empty()) {
     server_id_ = "1";
+  } else if (65535 < std::stoull(server_id_)) {
+    server_id_ = "65535";
   }
   GetConfStr("requirepass", &requirepass_);
   GetConfStr("masterauth", &masterauth_);
