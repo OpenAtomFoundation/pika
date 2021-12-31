5ea6a7740245fd320a1025dc5155f6d5eb083acb
Ted Mostly <wanghenshui@qq.com>
2021-12-31 11:24:16 +0800

    fix max-write-buffer-num mismatch (#1130)

--- src/pika_conf.cc
+++ src/pika_conf.cc
@@ -561,7 +561,7 @@ int PikaConf::ConfigRewrite() {
   // options for storage engine
   SetConfInt("max-cache-files", max_cache_files_);
   SetConfInt("max-background-compactions", max_background_compactions_);
-  SetConfInt("max-write-buffer-number", max_write_buffer_num_);
+  SetConfInt("max-write-buffer-num", max_write_buffer_num_);
   SetConfInt64("write-buffer-size", write_buffer_size_);
   SetConfInt64("arena-block-size", arena_block_size_);
   // slaveof config item is special
