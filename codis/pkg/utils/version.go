7a53ef6c17d9ba4d70877572ef524b6822ff502f
Wyatt Jia <i@eventloop.live>
2023-02-25 18:07:11 +0800

    [Codis]Unignore pkg utils version file.
    Signed-off-by: Wyatt Jia <i@eventloop.live>

---
+++ codis/pkg/utils/version.go
@@ -0,0 +1,6 @@
+package utils
+
+const (
+	Version = "2018-11-04 16:22:35 +0800 @de1ad026e329561c22e2a3035fbfe89dc7fef764 @3.2.2-12-gde1ad026"
+	Compile = "2023-02-23 11:25:09 +0800 by go version go1.19.6 linux/amd64"
+)
