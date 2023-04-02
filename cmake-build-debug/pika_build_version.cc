7faae4eac378ba358043b9f223c7a94fd7fd712b
alexstocks <alexstocks@foxmail.com>
2023-04-02 12:20:19 +0800

    modify git ignore

---
+++ cmake-build-debug/pika_build_version.cc
@@ -0,0 +1,10 @@
+// Copyright (c) 2015-present, Qihoo, Inc.  All rights reserved.
+// This source code is licensed under the BSD-style license found in the
+// LICENSE file in the root directory of this source tree. An additional grant
+// of patent rights can be found in the PATENTS file in the same directory.
+
+#include "include/build_version.h"
+const char* pika_build_git_sha =
+              "pika_git_sha:e44504843acbc2412556b65affc4545ea69e9247";
+const char* pika_build_git_date = "pika_build_git_date:2023-03-31 12:58:34";
+const char* pika_build_compile_date = "pika_build_date:2023-04-01 10:37:18";
