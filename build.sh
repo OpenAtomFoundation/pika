de3ad116472d6ce07657a5362bc9768b155a92da
richie <rliuqian@gmail.com>
2023-04-11 19:58:43 +0800

    Update build.sh (#1380)
    mac 无/proc/cpuinfo 文件处理

--- build.sh
+++ build.sh
@@ -97,7 +97,11 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
-CPU_CORE=`cat /proc/cpuinfo| grep "processor"| wc -l`
+if [ ! -f "/proc/cpuinfo" ];then
+  CPU_CORE=$(sysctl -n hw.ncpu)
+else
+  CPU_CORE=$(cat /proc/cpuinfo| grep "processor"| wc -l)
+fi
 if [ ${CPU_CORE} -eq 0 ]; then
   CPU_CORE=1
 fi
