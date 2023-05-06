052c739b63973f89336f7c8a89b153efa0b3169f
lqx <1239721421@qq.com>
2023-05-07 00:26:11 +0800

    add format file .cc

--- format_code.sh
+++ format_code.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-find include src pika-tools -regex '.*\.\(cpp\|hpp\|c\|h\)' | xargs clang-format -i
+find include src pika-tools -regex '.*\.\(cpp\|hpp\|c\|h\|cc\)' | xargs clang-format -i
 
 
 # If you want to automatically format your code before git commit
 # append the code to .git/hooks/pre-commit
 #
-# for FILE in $(git diff --cached --name-only | grep -E '.*\.(cpp|hpp|c|h)')
+# for FILE in $(git diff --cached --name-only | grep -E '.*\.(cpp|hpp|c|h|cc)')
 # do
 #    if [[ "$FILE" =~ .*.(cpp|hpp|c|h)$ ]];then
 #      clang-format -i $FILE
