48e135fb11798cf15b9abbb4142886a738243c04
liuyuecai <liuyuecai@360.cn>
2023-05-19 09:23:55 +0800

    修改执行脚本

--- integrate_test.sh
+++ integrate_test.sh
@@ -37,5 +37,6 @@ echo "github pull request head branch -> ${GITHUB_HEAD_REF}"
 
 git clone -b master https://github.com/luky116/pika-integration.git pika-integration && cd pika-integration
 
+chmod +x ./start_integrate_test.sh
 # start integrate test
 ./start_integrate_test.sh
