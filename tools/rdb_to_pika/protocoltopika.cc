e919de2b48b81de451cee2d32a69481749d610fc
Hao Lee <1838249551@qq.com>
2023-05-26 10:04:48 +0800

    fix:build ci error
    Signed-off-by: Hao Lee <1838249551@qq.com>

--- tools/rdb_to_pika/protocoltopika.cc
+++ tools/rdb_to_pika/protocoltopika.cc
@@ -29,7 +29,7 @@ int main(int argc, char** argv) {
   int port;
   ss >> port;
   redisContext* c = redisConnect(ip, port);
-  if (!c || c->err)) {
+  if (!c || c->err) {
     if (c) {
       redisFree(c);
       std::cout << "connection error" << std::endl;
