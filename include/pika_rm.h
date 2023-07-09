503205c80fe8222c8037a2712ad17b7335c1bba7
luky116 <luky116@126.com>
2023-07-09 23:56:05 +0800

    fix

--- include/pika_rm.h
+++ include/pika_rm.h
@@ -147,8 +147,6 @@ class SyncSlaveSlot : public SyncSlot {
 
   const std::string& MasterIp();
 
-  const std::string& MasterRunId();
-
   int MasterPort();
 
   void SetMasterSessionId(int32_t session_id);
