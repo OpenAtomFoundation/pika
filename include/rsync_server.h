afd133a8131cc7ec163d6bb1336f4d93c9c123a4
wangshaoyi <wangshaoyi@meituan.com>
2023-07-24 20:21:57 +0800

    add copyright

--- include/rsync_server.h
+++ include/rsync_server.h
@@ -1,3 +1,8 @@
+// Copyright (c) 2023-present, Qihoo, Inc.  All rights reserved.
+// This source code is licensed under the BSD-style license found in the
+// LICENSE file in the root directory of this source tree. An additional grant
+// of patent rights can be found in the PATENTS file in the same directory.
+
 #ifndef RSYNC_SERVER_H_
 #define RSYNC_SERVER_H_
 #include <stdio.h>
