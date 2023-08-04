b3c5268c271fd4932534992d3af40bcac8d97f77
chejinge <945997690@qq.com>
2023-08-04 21:59:10 +0800

    atom->3.5 (#1887)
    * Revised Timer Task Thread (#1862)
    
    * add TimertaskManager, removed TimedscanThread.
    
    * Rsync client support multi thread (#1866)
    
    * define rsync related header file and proto
    
    ---------
    
    Co-authored-by: cheniujh <41671101+cheniujh@users.noreply.github.com>
    Co-authored-by: wangshao1 <30471730+wangshao1@users.noreply.github.com>

--- src/net/include/server_thread.h
+++ src/net/include/server_thread.h
@@ -175,6 +175,7 @@ class ServerThread : public Thread {
 
   // process events in notify_queue
   virtual void ProcessNotifyEvents(const NetFiredEvent* pfe);
+  
 
   const ServerHandle* handle_;
   bool own_handle_ = false;
