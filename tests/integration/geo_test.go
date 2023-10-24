ac471e5c1e38fc838d143ba9626edb2f8d30268c
dingxiaoshuai123 <2486016589@qq.com>
2023-10-24 09:33:51 +0800

    update geo test

--- tests/integration/geo_test.go
+++ tests/integration/geo_test.go
@@ -112,17 +112,11 @@ var _ = Describe("Geo Commands", func() {
 			Expect(dist).To(BeNumerically("~", 166274.15, 0.01))
 		})
 
-		PIt("should get geo hash in string representation", func() {
-			res := client.Do(ctx, "GEOHASH", "Sicily", "Palermo", "Catania")
-			Expect(res.Err()).NotTo(HaveOccurred())
-			Expect(res.Val()).To(HaveLen(2))
-		})
-
-		PIt("should get geo hash in string representation", func() {
-			hashes, err := client.GeoHash(ctx, "Sicily", "Palermo", "Catania").Result()
-			Expect(err).NotTo(HaveOccurred())
-			Expect(hashes).To(ConsistOf([]string{"sqc8b49rny0", "sqdtr74hyu0"}))
-		})
+		//It("should get geo hash in string representation", func() {
+		//	res := client.Do(ctx, "GEOHASH", "Sicily", "Palermo", "Catania")
+		//	Expect(res.Err()).NotTo(HaveOccurred())
+		//	Expect(res.Val()).To(HaveLen(2))
+		//})
 
 		It("should return geo position", func() {
 			pos, err := client.GeoPos(ctx, "Sicily", "Palermo", "Catania", "NonExisting").Result()
