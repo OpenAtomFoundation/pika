c1fcb13b1159b0c48c6c38bdbd821ea42c02d4fe
chejinge <chejinge@360.cn>
2023-11-27 17:32:00 +0800

    fix

--- src/pika_slot.cc
+++ src/pika_slot.cc
@@ -107,15 +107,11 @@ std::shared_ptr<storage::Storage> Slot::db() const { return db_; }
 std::shared_ptr<PikaCache> Slot::cache() const { return cache_; }
 
 void Slot::Init() {
-  auto cache = std::make_shared<PikaCache>(g_pika_conf->zset_cache_start_pos(), g_pika_conf->zset_cache_field_num_per_key(), shared_from_this());
+  cache_ = std::make_shared<PikaCache>(g_pika_conf->zset_cache_start_pos(), g_pika_conf->zset_cache_field_num_per_key(), shared_from_this());
   // Create cache
   cache::CacheConfig cache_cfg;
   g_pika_server->CacheConfigInit(cache_cfg);
-  rocksdb::Status ret = cache->Init(g_pika_conf->GetCacheNum(), &cache_cfg);
-  assert(cache_);
-  assert(ret.ok());
-  LOG(INFO) << "Cache Success";
-  cache_ = std::move(cache);
+  cache_->Init(g_pika_conf->GetCacheNum(), &cache_cfg);
 }
 
 void Slot::Compact(const storage::DataType& type) {
