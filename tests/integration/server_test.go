e2d8a40b73b3df765e523b837f06292cf5850219
hero-heng <2374114527@qq.com>
2023-11-28 13:27:03 +0800

    feat:unstable branch

--- tests/integration/server_test.go
+++ tests/integration/server_test.go
@@ -390,11 +390,30 @@ var _ = Describe("Server", func() {
 		//	Expect(info.Val()).To(ContainSubstring(`memory`))
 		//})
 		//
-		//It("should LastSave", func() {
-		//	lastSave := client.LastSave(ctx)
-		//	Expect(lastSave.Err()).NotTo(HaveOccurred())
-		//	Expect(lastSave.Val()).NotTo(Equal(0))
-		//})
+		It("should LastSave", func() {
+			lastSave := client.LastSave(ctx)
+			Expect(lastSave.Err()).NotTo(HaveOccurred())
+			Expect(lastSave.Val()).NotTo(Equal(0))
+
+			mset := client.MSet(ctx, "key", "Hello", "key1", "Hello1")
+            Expect(lastSave.Err()).NotTo(HaveOccurred())
+            Expect(lastSave.Val()).NotTo(Equal("OK"))
+
+            mGet := client.MGet(ctx, "key", "key1")
+            Expect(mGet.Err()).NotTo(HaveOccurred())
+            Expect(mGet.Val()).To(Equal([]interface{}{"Hello", "Hello1"}))
+
+            bgSave := client.BgSave(ctx).Result()
+            bgSaveTime := time.Now().Unix()
+            Expect(bgSave).NotTo(HaveOccurred())
+            Expect(bgSave).To(ContainSubstring("Background saving started"))
+
+            lastSave := client.LastSave(ctx)
+            Expect(lastSave.Err()).NotTo(HaveOccurred())
+            myBgsaveTime int64 = bgSaveTime
+            Expect(lastSave.Val()).NotTo(Equal(int64(myBgsaveTime))
+            // Missing lastsave persistence test
+		})
 
 		//It("should Save", func() {
 		//
