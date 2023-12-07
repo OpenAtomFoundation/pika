5f628b1fd6e1b0b6063b0b78de6926121f2385f1
Tao_ta <745103130@qq.com>
2023-12-07 14:06:09 +0800

    fix: have no status return values from RocksDB

--- src/pika_set.cc
+++ src/pika_set.cc
@@ -256,8 +256,12 @@ void SRemCmd::DoInitial() {
 }
 
 void SRemCmd::Do(std::shared_ptr<Slot> slot) {
-  s_ = slot->db()->SRem(key_, members_, &deleted_);
-  res_.AppendInteger(deleted_);
+  rocksdb::Status s = slot->db()->SRem(key_, members_, &deleted_);
+  if (s.ok() || s.IsNotFound()) {
+    res_.AppendInteger(deleted_);
+  } else {
+    res_.SetRes(CmdRes::kErrOther, s.ToString());
+  }
 }
 
 void SRemCmd::DoThroughDB(std::shared_ptr<Slot> slot) {
@@ -282,11 +286,15 @@ void SUnionCmd::DoInitial() {
 
 void SUnionCmd::Do(std::shared_ptr<Slot> slot) {
   std::vector<std::string> members;
-  slot->db()->SUnion(keys_, &members);
-  res_.AppendArrayLenUint64(members.size());
-  for (const auto& member : members) {
-    res_.AppendStringLenUint64(member.size());
-    res_.AppendContent(member);
+  rocksdb::Status s = slot->db()->SUnion(keys_, &members);
+  if (s.ok() || s.IsNotFound()) {
+    res_.AppendArrayLenUint64(members.size());
+    for (const auto& member : members) {
+      res_.AppendStringLenUint64(member.size());
+      res_.AppendContent(member);
+    }
+  }else{
+    res_.SetRes(CmdRes::kErrOther, s.ToString());
   }
 }
 
@@ -374,11 +382,15 @@ void SInterCmd::DoInitial() {
 
 void SInterCmd::Do(std::shared_ptr<Slot> slot) {
   std::vector<std::string> members;
-  slot->db()->SInter(keys_, &members);
-  res_.AppendArrayLenUint64(members.size());
-  for (const auto& member : members) {
-    res_.AppendStringLenUint64(member.size());
-    res_.AppendContent(member);
+  rocksdb::Status s = slot->db()->SInter(keys_, &members);
+  if (s.ok() || s.IsNotFound()) {
+    res_.AppendArrayLenUint64(members.size());
+    for (const auto& member : members) {
+      res_.AppendStringLenUint64(member.size());
+      res_.AppendContent(member);
+    }
+  }else{
+    res_.SetRes(CmdRes::kErrOther, s.ToString());
   }
 }
 
@@ -469,11 +481,15 @@ void SDiffCmd::DoInitial() {
 
 void SDiffCmd::Do(std::shared_ptr<Slot> slot) {
   std::vector<std::string> members;
-  slot->db()->SDiff(keys_, &members);
-  res_.AppendArrayLenUint64(members.size());
-  for (const auto& member : members) {
-    res_.AppendStringLenUint64(member.size());
-    res_.AppendContent(member);
+  rocksdb::Status s = slot->db()->SDiff(keys_, &members);
+  if (s.ok() || s.IsNotFound()){
+    res_.AppendArrayLenUint64(members.size());
+    for (const auto& member : members) {
+      res_.AppendStringLenUint64(member.size());
+      res_.AppendContent(member);
+    }
+  }else{
+    res_.SetRes(CmdRes::kErrOther,s.ToString());
   }
 }
 
