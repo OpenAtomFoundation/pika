94e85a5f960e6da506c51a57caab0d6abfaf8ee4
JayLiu <38887641+luky116@users.noreply.github.com>
2024-03-08 21:49:35 +0800

    Integration tests differentiate between cached and non-cached (#2467)
    Co-authored-by: liuyuecai <liuyuecai@360.cn>

--- tests/integration/txn_test.go
+++ tests/integration/txn_test.go
@@ -30,6 +30,9 @@ var _ = Describe("Text Txn", func() {
 	BeforeEach(func() {
 		txnClient = redis.NewClient(PikaOption(SINGLEADDR))
 		cmdClient = redis.NewClient(PikaOption(SINGLEADDR))
+
+		GlobalBefore(ctx, txnClient)
+		GlobalBefore(ctx, cmdClient)
 	})
 	Describe("test watch", func() {
 		It("basic watch", func() {
