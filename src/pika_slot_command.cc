649c7d40cfc7519b04f5f18150ca6d2f0f8875b0
chejinge <chejinge@360.cn>
2024-03-11 11:49:04 +0800

    fix

--- src/pika_slot_command.cc
+++ src/pika_slot_command.cc
@@ -877,6 +877,44 @@ int GetKeyType(const std::string& key, std::string& key_type, const std::shared_
   return 1;
 }
 
+// delete key from db
+static int DeleteKey(const std::string& key, const char key_type, const std::shared_ptr<DB>& db) {
+  int32_t res = 0;
+  std::string slotKey = GetSlotKey(GetSlotID(key));
+
+  std::vector<std::string> members;
+  members.emplace_back(key_type + key);
+  // delete from cache
+  if (PIKA_CACHE_NONE != g_pika_conf->cache_model()
+      && PIKA_CACHE_STATUS_OK == db->cache()->CacheStatus()) {
+    db->cache()->Del(members);
+  }
+
+  // delete key from slot
+  rocksdb::Status s = db->storage()->SRem(slotKey, members, &res);
+  if (!s.ok()) {
+    if (s.IsNotFound()) {
+      LOG(INFO) << "Del key Srem key " << key << " not found";
+      return 0;
+    } else {
+      LOG(WARNING) << "Del key Srem key: " << key << " from slotKey, error: " << strerror(errno);
+      return -1;
+    }
+  }
+
+  // delete key from db
+  members.clear();
+  members.emplace_back(key);
+  std::map<storage::DataType, storage::Status> type_status;
+  int64_t del_nums = db->storage()->Del(members, &type_status);
+  if (0 > del_nums) {
+    LOG(WARNING) << "Del key: " << key << " at slot " << GetSlotID(key) << " error";
+    return -1;
+  }
+
+  return 1;
+}
+
 // get slotstagkey by key
 std::string GetSlotsTagKey(uint32_t crc) {
   return SlotTagPrefix + std::to_string(crc);
