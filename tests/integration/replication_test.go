e02930ad994a6d163fcb05c6717b7f0adbcc31ea
liuyuecai <liuyuecai@360.cn>
2024-03-14 20:06:09 +0800

    fix Eventually timeout

--- tests/integration/replication_test.go
+++ tests/integration/replication_test.go
@@ -430,10 +430,10 @@ var _ = Describe("should replication ", func() {
 			Expect(err1).NotTo(HaveOccurred())
 			Eventually(func() error {
 				return clientMaster.Get(ctx, "key").Err()
-			}, "65s", "100ms").Should(Equal(redis.Nil))
+			}, "20s", "100ms").Should(Equal(redis.Nil))
 			Eventually(func() error {
 				return clientSlave.Get(ctx, "key").Err()
-			}, "65s", "100ms").Should(Equal(redis.Nil))
+			}, "20s", "100ms").Should(Equal(redis.Nil))
 			log.Println("Replication test 1 success")
 
 			log.Println("Replication test 2 start")
@@ -446,10 +446,10 @@ var _ = Describe("should replication ", func() {
 			Expect(clientMaster.FlushDB(ctx).Err()).NotTo(HaveOccurred())
 			Eventually(func() error {
 				return clientMaster.Get(ctx, "x").Err()
-			}, "1s", "100ms").Should(Equal(redis.Nil))
+			}, "20s", "100ms").Should(Equal(redis.Nil))
 			Eventually(func() error {
 				return clientSlave.Get(ctx, "x").Err()
-			}, "1s", "100ms").Should(Equal(redis.Nil))
+			}, "20s", "100ms").Should(Equal(redis.Nil))
 			log.Println("Replication test 2 success")
 
 			log.Println("rpoplpush test start")
