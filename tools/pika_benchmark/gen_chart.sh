4715e0606e52cdf648055103ba9b70924599402f
liuyuecai <liuyuecai@360.cn>
2024-05-21 22:09:50 +0800

    change /usr/bin/python3 to python3

--- tools/pika_benchmark/gen_chart.sh
+++ tools/pika_benchmark/gen_chart.sh
@@ -2,7 +2,7 @@
 
 mkdir -p $(pwd)/charts
 
-/usr/bin/python3  gen_chart.py --filePath $(pwd)/parsed_data/cmd_ops --prefix=cmd --outPath=$(pwd)/charts
-/usr/bin/python3  gen_chart.py --filePath $(pwd)/parsed_data/cmd_latency --prefix=cmd --outPath=$(pwd)/charts
-/usr/bin/python3  gen_chart.py --filePath $(pwd)/parsed_data/rw_ops --prefix=rw --outPath=$(pwd)/charts
-/usr/bin/python3  gen_chart.py --filePath $(pwd)/parsed_data/rw_latency --prefix=rw --outPath=$(pwd)/charts
+python3  gen_chart.py --filePath $(pwd)/parsed_data/cmd_ops --prefix=cmd --outPath=$(pwd)/charts
+python3  gen_chart.py --filePath $(pwd)/parsed_data/cmd_latency --prefix=cmd --outPath=$(pwd)/charts
+python3  gen_chart.py --filePath $(pwd)/parsed_data/rw_ops --prefix=rw --outPath=$(pwd)/charts
+python3  gen_chart.py --filePath $(pwd)/parsed_data/rw_latency --prefix=rw --outPath=$(pwd)/charts
