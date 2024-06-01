b3af51c1d76054a31eaf9f32f7e5ff1970ebe978
chienguo <2778180460@qq.com>
2024-06-01 08:55:27 +0800

    remove assign

--- codis/pkg/proxy/session.go
+++ codis/pkg/proxy/session.go
@@ -370,13 +370,12 @@ func (s *Session) handleCodisInfo(r *Request) error {
 		return nil
 	}
 
-	array := []*redis.Resp{
+	r.Resp = redis.NewArray([]*redis.Resp{
 		redis.NewString([]byte(utils.Version)),
 		redis.NewString([]byte(utils.Compile)),
 		redis.NewString([]byte(fmt.Sprintf("admin addr: %s", s.proxy.model.AdminAddr))),
 		redis.NewString([]byte(fmt.Sprintf("start time: %s", s.proxy.model.StartTime))),
-	}
-	r.Resp = redis.NewArray(array)
+	})
 
 	return nil
 }
