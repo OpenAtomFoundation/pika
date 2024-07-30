f8ab8a3269317f577f1da518a39e7af971dd3d83
cheniujh <1271435567@qq.com>
2024-07-30 10:02:41 +0800

    removed some comments

--- include/pika_repl_client.h
+++ include/pika_repl_client.h
@@ -85,9 +85,6 @@ class PikaReplClient {
   int next_avail_ = 0;
   std::hash<std::string> str_hash;
   std::vector<std::unique_ptr<PikaReplBgWorker>> write_binlog_workers_;
-  //[NOTICE] the task queue of WriteDBWorker must never be deliberately cleared,
-  // because their queue size are related with  ConsensusCoordinator::async_write_db_task_count_
-  // check PR # /disscussion #2807 to know more
   std::vector<std::unique_ptr<PikaReplBgWorker>> write_db_workers_;
 };
 
