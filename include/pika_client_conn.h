578fe41d7c2d986fe9da93ff5b1efd2e2bd3434c
cheniujh <41671101+cheniujh@users.noreply.github.com>
2024-07-31 15:37:30 +0800

    feat: use RTC(Run-to-completion) model to speed up cache read (#2837)
    * feat: Improve the RTC process of Read/Write model  (#2629)
    
    * (Demo) Do read cmd before task queue. && add workflow_dispatch for manual action
    
    * Check authed and write lock, fix go test error in MacOS and cache mode judge
    
    * fix some ut error by  commands filter  and return logic
    
    * rollback some flag,but add kCmdReadBeforeQueuefor get mget hget hget hgetall,hmget
    
    * move mget and hmget;add before_queue_time metrics
    
    * fix cost to copy cmd_table by remove c_ptr
    
    ---------
    
    Co-authored-by: chenbt <34958405+chenbt-hz@users.noreply.github.com>

--- include/pika_client_conn.h
+++ include/pika_client_conn.h
@@ -19,6 +19,7 @@ struct TimeStat {
   void Reset() {
     enqueue_ts_ = dequeue_ts_ = 0;
     process_done_ts_ = 0;
+    before_queue_ts_ = 0;
   }
 
   uint64_t start_ts() const {
@@ -37,8 +38,13 @@ struct TimeStat {
     return process_done_ts_ > dequeue_ts_ ? process_done_ts_ - dequeue_ts_ : 0;
   }
 
+  uint64_t before_queue_time() const {
+    return process_done_ts_ > dequeue_ts_ ? before_queue_ts_ - enqueue_ts_ : 0;
+  }
+
   uint64_t enqueue_ts_;
   uint64_t dequeue_ts_;
+  uint64_t before_queue_ts_;
   uint64_t process_done_ts_;
 };
 
@@ -67,8 +73,11 @@ class PikaClientConn : public net::RedisConn {
                  const net::HandleType& handle_type, int max_conn_rbuf_size);
   ~PikaClientConn() = default;
 
+  bool IsInterceptedByRTC(std::string& opt);
+
   void ProcessRedisCmds(const std::vector<net::RedisCmdArgsType>& argvs, bool async, std::string* response) override;
 
+  bool ReadCmdInCache(const net::RedisCmdArgsType& argv, const std::string& opt);
   void BatchExecRedisCmd(const std::vector<net::RedisCmdArgsType>& argvs);
   int DealMessage(const net::RedisCmdArgsType& argv, std::string* response) override { return 0; }
   static void DoBackgroundTask(void* arg);
