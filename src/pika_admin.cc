1df3351528b84b948997734fd2424c54d94c110d
cheniujh <1271435567@qq.com>
2024-08-09 18:04:53 +0800

    change the calculating logic of slave_repl_offset

--- src/pika_admin.cc
+++ src/pika_admin.cc
@@ -1219,8 +1219,6 @@ void InfoCmd::InfoReplication(std::string& info) {
   Status s;
   uint32_t filenum = 0;
   uint64_t offset = 0;
-  uint64_t total_file_num = 0;
-  uint64_t total_offset = 0;
   uint64_t slave_repl_offset = 0;
   std::string safety_purge;
   std::shared_ptr<SyncMasterDB> master_db = nullptr;
@@ -1233,17 +1231,12 @@ void InfoCmd::InfoReplication(std::string& info) {
       continue;
     }
     master_db->Logger()->GetProducerStatus(&filenum, &offset);
-    total_file_num += filenum;
-    total_offset += offset;
+    slave_repl_offset += static_cast<uint64_t>(filenum) * static_cast<uint64_t>(g_pika_conf->binlog_file_size());
+    slave_repl_offset += offset;
     tmp_stream << db_name << ":binlog_offset=" << filenum << " " << offset;
     s = master_db->GetSafetyPurgeBinlog(&safety_purge);
     tmp_stream << ",safety_purge=" << (s.ok() ? safety_purge : "error") << "\r\n";
   }
-  total_file_num += total_offset / static_cast<uint64_t>(g_pika_conf->binlog_file_size());
-  total_offset = total_offset % static_cast<uint64_t>(g_pika_conf->binlog_file_size());
-  //then total_offset will not greater than binlog file size whose max value is 2GB in bytes
-  //which can be contained by uint32_t
-  slave_repl_offset = (total_file_num << 32) | total_offset;
   tmp_stream << "slave_repl_offset:" << slave_repl_offset << "\r\n";
   info.append(tmp_stream.str());
 }
