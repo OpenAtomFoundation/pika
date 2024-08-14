84b234f6f7d258d49c4c693bf000bca645ce2851
chejinge <945997690@qq.com>
2024-08-14 14:46:54 +0800

    fix:fix 355 CI (#2864)
    * fix:fix 355 CI
    
    ---------
    
    Co-authored-by: chejinge <chejinge@360.cn>

--- tests/integration/start_master_and_slave.sh
+++ tests/integration/start_master_and_slave.sh
@@ -104,7 +104,8 @@ sed -i ''  \
   -e 's|dump-path : ./dump/|dump-path : ./master_rename_data/dump/|'  \
   -e 's|pidfile : ./pika.pid|pidfile : ./master_rename_data/pika.pid|'  \
   -e 's|db-sync-path : ./dbsync/|db-sync-path : ./master_rename_data/dbsync/|'  \
-  -e 's|#daemonize : yes|daemonize : yes|' ./pika_master_rename.conf
+  -e 's|#daemonize : yes|daemonize : yes|'  \
+  -e 's|timeout : 60|timeout : 500|' ./pika_master_rename.conf
 
 sed -i ''  \
   -e 's|# rename-command : FLUSHDB 360flushdb|rename-command : FLUSHDB 360flushdb|'  \
@@ -114,7 +115,8 @@ sed -i ''  \
   -e 's|dump-path : ./dump/|dump-path : ./slave_rename_data/dump/|'  \
   -e 's|pidfile : ./pika.pid|pidfile : ./slave_rename_data/pika.pid|'  \
   -e 's|db-sync-path : ./dbsync/|db-sync-path : ./slave_rename_data/dbsync/|'  \
-  -e 's|#daemonize : yes|daemonize : yes|' ./pika_slave_rename.conf
+  -e 's|#daemonize : yes|daemonize : yes|'  \
+  -e 's|timeout : 60|timeout : 500|' ./pika_slave_rename.conf
 
 # Start three nodes
 ./pika -c ./pika_single.conf
